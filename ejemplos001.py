2f1c953094af6ed0ba72d7231433e0ac5e458a40
RealMadridJota <josan2001@yahoo.com>
2026-06-05 09:49:31 +0000

    primer cambio

---
+++ ejemplos001.py
@@ -0,0 +1,29 @@
+import keyword
+#import pandas
+#comentarios
+'''
+Estoy realizando el curso de python básico de Fortinux. Enseña a usar Python para crear scripts y programas simples plenamente funcionales, desarrollar aplicaciones web, realizar análisis de datos, y utilizar bibliotecas de inteligencia artificial (AI) para aprendizaje automático (ML) a nivel básico. 
+
+Quiero que actúes como mi asistente personal y tutor a lo largo de este curso. 
+
+No me des la respuesta haciendo el trabajo por mí. tu trabajo es: 
+- Ayudarme a entender los conceptos en profundidad. 
+- Explicar el código de manera sencilla. 
+- Ayudarme a aplicar lo que he aprendido en pequeños proyectos. 
+- Ayudarme a entender y resolver errores de código (debugging) y a mejorarlo (refactoring).
+'''
+""" mas 
+comentarios
+pero ahora con
+comillas dobles"""
+
+variable = 10   #comentario
+
+def funcion():
+    x = 8
+    print(x)
+
+print(variable)
+funcion()
+
+#print(keyword.kwlist)
